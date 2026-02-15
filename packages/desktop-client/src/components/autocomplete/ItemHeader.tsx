46c01ac8e1be6c8ba3abc9343cc5f75a572a66f1
Juulz <julesmcn@gmail.com>
2026-02-15 10:56:06 -0500

    Update ItemHeader.tsx
    Use sidebar colors for autocomplete dropdowns

--- packages/desktop-client/src/components/autocomplete/ItemHeader.tsx
+++ packages/desktop-client/src/components/autocomplete/ItemHeader.tsx
@@ -24,7 +24,7 @@ export function ItemHeader({ title, style, type, ...props }: ItemHeaderProps) {
   return (
     <div
       style={{
-        color: theme.menuAutoCompleteTextHeader,
+        color: theme.sidebarItemTextSelected,
         padding: '4px 9px',
         ...narrowStyle,
         ...style,
